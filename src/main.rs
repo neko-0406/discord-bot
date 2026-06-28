e9ea159febaad931c996b81f30a7ed17abcb4369
momiji <sateraito3696@gmail.com>
2026-06-28 16:34:31 +0900

    設定ファイルを相対パスに変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::atomic::AtomicBool;
 use serenity::Client;
 use serenity::prelude::GatewayIntents;
 use crate::events::ping_event::PingEvent;
@@ -6,22 +7,25 @@ use crate::setting::BotSetting;
 
 mod events;
 mod setting;
+mod modules;
 
 #[tokio::main]
 async fn main() {
     // 設定ファイルの読み込み処理
-    let setting_path = "E:/develop/discord-bot/setting.json";
+    let setting_path = "./setting.json";
     let load_setting_result = BotSetting::read_setting_file(&setting_path);
     
     // 設定ファイルの読み込み成功
     if let Some(setting) = load_setting_result {
         let bot_token = setting.discord_token.as_str();
         let intents = 
+            GatewayIntents::GUILDS |
             GatewayIntents::GUILD_MESSAGES |
             GatewayIntents::MESSAGE_CONTENT;
-        
+
+        // クライアントオブジェクトの作成
         let client = Client::builder(bot_token, intents)
-            .event_handler(ReadyEvent)
+            .event_handler(ReadyEvent {issued_task: AtomicBool::new(false)})
             .event_handler(PingEvent)
             .await;
         
@@ -33,13 +37,5 @@ async fn main() {
                 println!("Failed to start this bot:{}", e.to_string());
             }
         }
-        // 失敗
-        else {
-            println!("Failed to create Client");
-        }
-    }
-    // 設定ファイルの読み込み失敗
-    else {
-        println!("Failed to read setting file");
     }
 }
